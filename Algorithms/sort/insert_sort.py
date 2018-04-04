cb1ac49b5ced97d3ea459e1b8ab06d23cbb872c1
Mr.Child <lishunsheng2012@gmail.com>
2018-04-04 21:59:11 +0900

    add insert_sort

---
+++ Algorithms/sort/insert_sort.py
@@ -0,0 +1,17 @@
+﻿def insert_sort(lst):
+    """
+    插入排序
+    """
+    length = len(lst)
+
+    if length <= 1:
+        return lst
+
+    for x in range(1, length):
+        for y in range(x, 0, -1):
+            if lst[y] < lst[y - 1]:
+                lst[y], lst[y - 1] = lst[y - 1], lst[y]
+            else:
+                break
+
+    return lst
