b28cd0e6567e1965508dc3a55738868c8a3ab54b
Mr.Child <lishunsheng2012@gmail.com>
2018-04-06 22:21:49 +0900

    add radix_sort

---
+++ Algorithms/sort/radix_sort.py
@@ -0,0 +1,18 @@
+﻿import math
+
+
+def radix_sort(lst, radix=10):
+    """
+    基数排序
+    """
+    k = int(math.ceil(math.log(max(lst), radix)))
+    bucket = [[] for _ in range(radix)]
+
+    for i in range(1, k + 1):
+        for j in lst:
+            bucket[j % (radix ** i) // radix ** (i - 1)].append(j)
+        del lst[:]
+        for z in bucket:
+            lst.extend(z)
+
+    return lst
