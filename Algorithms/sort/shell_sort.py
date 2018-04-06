62a7b82f92230bd6955d517979a0cbb902ec03d0
Mr.Child <lishunsheng2012@gmail.com>
2018-04-06 22:20:13 +0900

    add shell_sort

---
+++ Algorithms/sort/shell_sort.py
@@ -0,0 +1,22 @@
+﻿def shell_sort(lst):
+    """
+    希尔排序
+    """
+    length = len(lst)
+    # 初始步长
+    gap = length // 2
+
+    while gap > 0:
+        for x in range(gap, length):
+            # 每个步长进行插入排序
+            tmp = lst[x]
+            y = x
+            # 插入排序
+            while y >= gap and lst[y - gap] > tmp:
+                lst[y] = lst[y - gap]
+                y -= gap
+            lst[y] = tmp
+        # 得到新的步长
+        gap = gap // 2
+
+    return lst
